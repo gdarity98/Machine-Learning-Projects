4582489186d2b83fe657d0662566b457eaf44172
davemilller <54906029+davemilller@users.noreply.github.com>
2020-09-01 13:17:36 -0600

    Cleaned up BreastCancerData, made IrisData, and tested it in DataMain.

--- project_1/BreastCancerData.java
+++ project_1/BreastCancerData.java
@@ -1,13 +1,10 @@
 package project_1;
 
 public class BreastCancerData {
-	private String allData = " ";
-	public String id = " ";
-	//private int section = 0;
-	//private String seats = " ";
-	//private String title = " ";
-	//private String when = " ";
-	//private String lecture = " ";
+	private String allData;
+	public String id;
+	public String classNo;	//this is a 2 or a 4 (benign / malignant)
+	public int[] features;			//feature array (1 - 9)
 	
 	public BreastCancerData(String fullData) {
 		//This sets up all the variables by splitting the string and assigning the correct value to variables.
@@ -19,14 +16,13 @@ public class BreastCancerData {
 		//WHILE data is being separated we can filter out unknown items and change them to what we need them to be.
 		
 		id = tokens[0];
-		//when = (tokens[15] + "-" + tokens[16]);
-		//lecture = tokens[14];
-		//seats = tokens[4];
-		//title = tokens[2];
-		
-		
-		//check if it is a lecture or a lab
-		//walk through the string and separate it and store necessary data
+		classNo = tokens[10];
+
+		features = new int[9];
+		for (int i= 0; i < 9; i++) {
+			features[i]= Integer.parseInt(tokens[i+1]);
+		}
+
 	}
 	
 	//returns full info variable
@@ -38,42 +34,14 @@ public class BreastCancerData {
 	public String getID() {
 		return id;
 	}
-//	
-//	//returns the section variable
-//	public int getSection() {
-//		return section;
-//	}
-//	
-//	//increments the section variable by 1
-//	public void sectionIncrement() {
-//		section++;
-//	}
-//	public void sectionIncrement(MSUClass val) {
-//		section += val.getSection();
-//	}
-//	
-//	
-//	public String getSeats() {
-//		return seats;
-//	}
-//	
-//	public void setSeats(String newVal) {
-//		Integer seatsInt = Integer.valueOf(seats);
-//		Integer newValInt = Integer.valueOf(newVal);
-//		seatsInt += newValInt;
-//		seats = Integer.toString(seatsInt);
-//	}
-//	
-//	public String getTitle() {
-//		return title;
-//	}
-//	
-//	public String getWhen() {
-//		return when;
-//	}
-//	
-//	public String getLecture() {
-//		return lecture;
-//	}
-	
+
+	//returns class number (2 or 4)
+	public String getClassNo() {
+		return classNo;
+	}
+
+	//returns feature array
+	public int[] getFeatures() {
+		return features;
+	}
 }
--- project_1/DataMain.java
+++ project_1/DataMain.java
@@ -3,46 +3,75 @@ package project_1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+
 import project_1.BreastCancerData;
 import project_1.BinarySearchTree;
 import project_1.Queue;
 
 public class DataMain {
     public static void main(String[] args) {
-        BufferedReader reader; //creates a buffered reader
-        BreastCancerData bData; //creates the breast cancer data class
-        BinarySearchTree<String, BreastCancerData> st; //Creates a BST that will be full of breast cancer data
-        st = new BinarySearchTree();
-        try { //Reads in the file and checks for exception
-        	//The file path would need to be changed based on system.
-        	reader = new BufferedReader(new FileReader("/Users/gdari/Desktop/Important/School/CSCI 447/School_Class_Things/Data/breast-cancer-wisconsin.data"));
-        	//reads the file in line by line
-        	String line = reader.readLine();
-        	//While we are not at the end of the file do things in the while loop
-        	while(line != null) {
-        		//This creates a bData class with the one line from the file
-        		bData = new BreastCancerData(line);
-
-        		st.put(bData.getID(), bData);
-        		line = reader.readLine();
-        	}
-        	
-        }catch (IOException e) {
-        	e.printStackTrace();
-        }
-        
-        //Prints everything in tree
-        st.keys();  //This is a changed keys method to print depthFirst
-       
-        //This is how we would get to specific data
-        BreastCancerData oneData = st.get(Integer.toString(95719));
-        System.out.println(oneData.getFullInfo());
+//        BufferedReader reader; //creates a buffered reader
+//        BreastCancerData bData; //creates the breast cancer data class
+//        BinarySearchTree<String, BreastCancerData> st; //Creates a BST that will be full of breast cancer data
+//        st = new BinarySearchTree();
+//        try { //Reads in the file and checks for exception
+//        	//The file path would need to be changed based on system.
+//        	reader = new BufferedReader(new FileReader("data-sets/breast-cancer-wisconsin.data"));
+//        	//reads the file in line by line
+//        	String line = reader.readLine();
+//        	//While we are not at the end of the file do things in the while loop
+//        	while(line != null) {
+//        		//This creates a bData class with the one line from the file
+//        		bData = new BreastCancerData(line);
+//
+//        		st.put(bData.getID(), bData);
+//        		line = reader.readLine();
+//        	}
+//
+//        }catch (IOException e) {
+//        	e.printStackTrace();
+//        }
+//
+//        //Prints everything in tree
+//        st.keys();  //This is a changed keys method to print depthFirst
+//
+//        //This is how we would get to specific data
+//        BreastCancerData oneData = st.get(Integer.toString(95719));
+//        System.out.println(oneData.getFullInfo());
         
         //To get to large amounts of data you would want to copy and change the .keys function or 
         //two other functions in order to get access to stuff while the tree is going through everything
         //For whatever reason if I wanted to sum the id numbers I would need to go into keys
         //then while it is going through add everything up while it does recursive calls
         //I'm sure we can figure it all out.
+
+
+        //------------------------------------------------------------------------------------------testing
+        BufferedReader reader1; //creates a buffered reader
+        IrisData iData; //creates the breast cancer data class
+        IrisData[] irisArray= new IrisData[150];
+
+        try { //Reads in the file and checks for exception
+            //The file path would need to be changed based on system.
+            reader1 = new BufferedReader(new FileReader("data-sets/iris.data"));
+            //reads the file in line by line
+            String line1 = reader1.readLine();
+            int lineNo = 0;
+            //While we are not at the end of the file do things in the while loop
+            while(line1 != null) {
+                //This creates a bData class with the one line from the file
+                iData = new IrisData(line1, ++lineNo);
+                irisArray[lineNo - 1] = iData;
+
+                System.out.println(lineNo + " " + Arrays.toString(iData.getBinnedFeatures()) + " " + iData.getClassNo());
+
+                line1 = reader1.readLine();
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
         
       
     }
