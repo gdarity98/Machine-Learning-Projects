c57b654f96b26f8bb9298af5cf7827acba472836
Dave Miller <superdave2347@gmail.com>
2020-09-02 22:40:52 -0600

    Added a loss() function to train, shows loss of iris-data and iris-noise

--- project_1/Train.java
+++ project_1/Train.java
@@ -11,12 +11,15 @@ import java.util.Arrays;
 public class Train {
 
     public IrisData[][] data = new IrisData[3][50];
+    public IrisData[] alldata = new IrisData[150];
+    public IrisData[] noiseData = new IrisData[150];
 
 
     public Train() {
         BufferedReader reader; //creates a buffered reader
         IrisData iData;
         IrisData[] irisArray= new IrisData[150];
+        IrisData[] irisArrayNoise = new IrisData[150];
 
         try { //Reads in the file and checks for exception
             reader = new BufferedReader(new FileReader("data-sets/iris.data"));
@@ -32,6 +35,18 @@ public class Train {
 //                System.out.println(lineNo + " " + Arrays.toString(iData.getBinnedFeatures()) + " " + iData.getClassNo());
 
                 line = reader.readLine();
+            }
+
+            //reads in the iris-noisy.txt and fills irisArrayNoise =
+            reader= new BufferedReader(new FileReader("data-sets/iris-noise.txt"));
+            line = reader.readLine();
+            lineNo = 0;
+
+            while (line != null) {
+                iData = new IrisData(line, lineNo + 1);
+                irisArrayNoise[lineNo++] = iData;
+
+                line= reader.readLine();
             }
 
         }catch (IOException e) {
@@ -57,6 +72,8 @@ public class Train {
         this.data[0] = setosa;
         this.data[1] = versicolor;
         this.data[2] = virginica;
+        this.alldata = irisArray;
+        this.noiseData = irisArrayNoise;
 
     }
 
@@ -75,7 +92,7 @@ public class Train {
                 num++;
             }
         }
-        System.out.println("Num: "+ num + ", feature: " + feature);
+//        System.out.println("Num: "+ num + ", feature: " + feature);
 
         return (num+1) / 154.0;
     }
@@ -95,24 +112,104 @@ public class Train {
             C[i]= probabilityOfClass * product;
         }
 
-        //return argmax
-        System.out.println(Arrays.toString(C));
+        //return argmax (this could be done cleaner)
+//        System.out.println(Arrays.toString(C));
         if (C[0] > C[1] && C[0] > C[2]) {
-            return "setosa";
+            return "Iris-setosa";
         }
         else if (C[1] > C[0] && C[1] > C[2]) {
-            return "versicolor";
+            return "Iris-versicolor";
         }
         else {
-            return "virginica";
+            return "Iris-virginica";
         }
     }
 
+    /*
+        Makes a confusion matrix
+            c1  c2  c3
+        c1  TP  FP
+        c2      TP
+        c3          TP
+
+        definitely confusing...
+     */
+    public double loss(IrisData[] testData) {
+        //confusion matrix ?
+        int count= 0;
+        int[][] confusionMatrix = new int[3][3];
+
+        for (IrisData irisData: testData) {
+            String trueClass = irisData.getClassNo();
+            int[] test = irisData.getBinnedFeatures();
+            String guess = classify(test);
+
+            if (trueClass.contentEquals("Iris-setosa") && guess.contentEquals(irisData.getClassNo())) {
+                count++;
+
+                confusionMatrix[0][0]++;
+                if (trueClass.contentEquals("Iris-versicolor")) {
+                    confusionMatrix[0][1]++;
+                }
+                else {
+                    confusionMatrix[0][2]++;
+                }
+            }
+            if (trueClass.contentEquals("Iris-versicolor") && guess.contentEquals(irisData.getClassNo())) {
+                count++;
+
+                confusionMatrix[1][1]++;
+                if (trueClass.contentEquals("Iris-versicolor")) {
+                    confusionMatrix[1][2]++;
+                }
+                else {
+                    confusionMatrix[1][0]++;
+                }
+            }
+            if (trueClass.contentEquals("Iris-virginica") && guess.contentEquals(irisData.getClassNo())) {
+                count++;
+
+                confusionMatrix[2][2]++;
+                if (trueClass.contentEquals("Iris-versicolor")) {
+                    confusionMatrix[2][0]++;
+                }
+                else {
+                    confusionMatrix[2][1]++;
+                }
+            }
+//            System.out.println("TA: "+ trueA + "\tTB: "+ trueB + "\tTC: "+ trueC+ "\nFA: "+ falseA+ "\tFB: "+ falseB+ "\tFC: "+ falseC);
+
+        }
+
+        double Pmacro= 0;
+        for (int[] row: confusionMatrix) {
+            System.out.println(Arrays.toString(row));
+            Pmacro += (double)row[0] / (row[0] + row[1] + row[2]);
+        }
+        Pmacro /= 3.0;
+        System.out.println("Pmacro: "+ Pmacro);
+
+        double Rmacro = 0;
+        for (int i= 0; i < 3; i++) {
+            Rmacro += (double)confusionMatrix[i][0] / (confusionMatrix[i][1] + confusionMatrix[i][2]);
+        }
+        Rmacro /= 3.0;
+        System.out.println("Rmacro: "+ Rmacro);
+
+        return 100*(count / 150.0);
+    }
+
     public static void main(String[] args) {
-        Train train= new Train();
-        int[] test= {2, 2, 3, 3};
-        System.out.println("Test data: "+ Arrays.toString(test));
-        System.out.println(train.classify(test));
+        Train train= new Train(); //Model()
+
+//        int[] test= {2, 2, 3, 3};
+//        System.out.println("Test data: "+ Arrays.toString(test));
+//        System.out.println(train.classify(test));
+
+        System.out.println("Loss: " + (int)train.loss(train.alldata) + "%");            //-->92%
+        System.out.println("Loss, noisy: " + (int)train.loss(train.noiseData)+ "%");    //-->90%
+
+
     }
 
 }
