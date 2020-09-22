bedce9e259a52603a9436cc3d743d6534729e104
Dave Miller <superdave2347@gmail.com>
2020-09-21 19:14:05 -0600

    KNN crossValidate() and tune()

--- project_2/project_2/KNearestNeighbor.java
+++ project_2/project_2/KNearestNeighbor.java
@@ -1,10 +1,6 @@
 package project_2;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 /*
     1. Load data
@@ -14,7 +10,9 @@ import java.util.Comparator;
         b. Store the distance and id of example in an array
     4. Sort the array by distance --> O(nlog(n))
     5. Take the K smallest elements
-    6. Return majority class as the classification of q
+    6.
+        a. Return majority class as the classification of q
+        b. Return 1/d weighted average of nearest neighbors for regression
 
     OR
 
@@ -25,7 +23,8 @@ import java.util.Comparator;
 public class KNearestNeighbor {
 
     public DataC[] data;
-    public int numClasses = 0;
+    public int numClasses;
+    public int k;
    
     /*
         Need to make constructor such that you can give it any of the data sets and
@@ -34,7 +33,7 @@ public class KNearestNeighbor {
     public KNearestNeighbor(DataC[] dataIn, int num) {
     	this.data = dataIn;
     	this.numClasses = num;
-
+        this.k = 1;
     }
 
     /*
@@ -59,19 +58,20 @@ public class KNearestNeighbor {
         in the data set, then finds its k nearest neighbors and classifies it based on a plurality
         vote from the neighbors' classes. (Working (I think))
      */
-    public String classify(double[] query) {
-        int k = 3;
+    public String classify(DataC[] dataSet, double[] query) {
         DataC[] nearestNeighbors = new DataC[k];
-        double[][] distArray = new double[data.length][2];
+        double[][] distArray = new double[dataSet.length][2];
 
         //fill distArray[i][0] w/ distance between query point and point i
         //fill distArray[i][1] w/ id of point i
         double dist;
         int i= 0;
-        for (DataC d: data) {
-            dist = getDistance(d.getFeatures(), query);
-            distArray[i][0] = dist;
-            distArray[i++][1] = d.getID();
+        for (DataC d: dataSet) {
+            if (d != null) {
+                dist = getDistance(d.getFeatures(), query);
+                distArray[i][0] = dist;
+                distArray[i++][1] = d.getID();
+            }
         }
 
         //sort distances using this madness to sort 2D array
@@ -86,8 +86,12 @@ public class KNearestNeighbor {
         });
 
         //fill nearestNeighbors[]
-        for (int j= 0; j< nearestNeighbors.length; j++) {
-            nearestNeighbors[j] = data[(int) distArray[j][1]-1];
+        int j= 0, n= 0;
+        while (n < nearestNeighbors.length) {
+            if (distArray[j][1] > 0) {
+                nearestNeighbors[n++] = data[(int) distArray[j][1]-1];
+            }
+            j++;
         }
 
         //print class labels of nearest neighbors
@@ -98,9 +102,8 @@ public class KNearestNeighbor {
         //take majority of nearest neighbor classes as max
         //w/ int vote[] where it increments vote[classNo]
         String cl;
-        int[] vote = new int[8];    //--> need to not hardwire this but Data object does not know numClasses
+        int[] vote = new int[numClasses+2];
         int max = -1;
-        boolean same = true;
         for (DataC nearestNeighbor : nearestNeighbors) {
             int classLabel = Integer.parseInt(nearestNeighbor.getClassLabel());
             vote[classLabel]++;
@@ -109,29 +112,113 @@ public class KNearestNeighbor {
             }
         }
 
-        System.out.println(Arrays.toString(vote));
+//        System.out.println(Arrays.toString(vote));
         cl = String.valueOf(max);
 
         return cl;
     }
 
     /*
-        L O S S --> test on whole data set
+        L O S S -->
      */
-    public void loss() {
+    public double loss(DataC[] dataSet, DataC[] testSet) {
         int count = 0;
-        for (DataC d: data) {
-            if (classify(d.getFeatures()).contentEquals(d.getClassLabel())) {
-                count++;
+        for (DataC d: testSet) {
+            if (d != null) {
+                String trueClass = d.getClassLabel();
+                double[] test = d.getFeatures();
+                if (classify(dataSet, test).contentEquals(trueClass)) {
+                    count++;
+                }
             }
         }
 
-        double accuracy = (double) count / data.length;
+        double accuracy = (double) count / testSet.length;
 
-        System.out.println("Accuracy: " + accuracy);
+        return accuracy;
+    }
+
+    public double crossValidate() {
+        int trainLen = (int)(0.9 * data.length)+1;
+        int testLen = data.length - trainLen;
+        DataC[] train = new DataC[trainLen];
+        DataC[] test;
+        DataC[][] split = new DataC[10][testLen];
+        double loss= 0;
+
+        //shuffle
+        List<DataC> temp = Arrays.asList(data);
+        Collections.shuffle(temp);
+        DataC[] shuffled = new DataC[data.length];
+        temp.toArray(shuffled);
+
+        //split into 10 arrays
+        int c= 0;
+        for (int i= 0; i< 10; i++) {
+            for (int j= 0; j< testLen; j++) {
+                if (c >= data.length) {
+                    split[i][j] = null;
+                }
+                else {
+                    split[i][j] = shuffled[c++];
+                }
+            }
+        }
+
+        //cross-validate 10x
+        for (int fold= 0; fold< 10; fold++) {
+
+            //train fold to (fold+8)%10, test on (fold+9)%10
+            List<DataC> list = new ArrayList<>();
+            for (int i= 0; i< 9; i++) {
+                //add 1/10 of data to list 9 times
+                list.addAll(Arrays.asList(split[(i+fold)%10]));
+            }
+
+            //fill train[] and test[]
+            list.toArray(train);
+            test = split[(fold+9)%10];
+
+            //get loss data
+            loss += loss(train, test);
+        }
+
+        //average loss data
+        loss /= 10;
+
+        System.out.println("Accuracy: " + loss);
+
+        return loss;
+    }
+
+    public void tune() {
+        double max= -1;
+        int bestK= 1;
+        double accuracy;
+
+        //test k up to 20, take best and set it
+        for (int k= 1; k <= 20; k++) {
+            this.k = k;
+
+            System.out.println("k = " + k);
+            accuracy = crossValidate();
+            if (accuracy > max) {
+                max = accuracy;
+                bestK = k;
+            }
+        }
+
+        System.out.println("Best k --> " + bestK);
+        this.k = bestK;
     }
     
     public static void main(String[] args) {
-    	
+        String gdFileName = "data-sets/glass.data";
+        DataSetUp gdSetUp = new DataSetUp(gdFileName, "end","classification");
+
+        KNearestNeighbor glassKNearestNeighbor = new KNearestNeighbor(gdSetUp.getAllData(), gdSetUp.numClasses());
+        glassKNearestNeighbor.tune();
+        glassKNearestNeighbor.crossValidate();
+//        System.out.println(glassKNearestNeighbor.loss(glassKNearestNeighbor.data, glassKNearestNeighbor.data));
     }
 }
