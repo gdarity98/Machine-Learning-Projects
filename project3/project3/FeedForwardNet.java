aa0a5f37a71ecbf46fdb04a11f0ca912203b8155
Dave Miller <superdave2347@gmail.com>
2020-10-11 23:14:53 -0600

    Layer class might work

--- project3/project3/FeedForwardNet.java
+++ project3/project3/FeedForwardNet.java
@@ -1,50 +1,101 @@
 package project3;
 
+import java.util.Arrays;
+
 /*
     Need to figure out how to make a nice architecture of a network
     Node class might work, but I will give it some thought.
  */
 public class FeedForwardNet {
 
-    private class Node {
-        private double[] in;
-        private double out;
+    /*
+        Layer describes a single layer in the network
 
-        public double feedForward() {
-            double sum = 0;
-            for (double d : in) {
-                sum += d;
+        NEED TO ADD
+            update nodes
+            update weights w/ backprop
+     */
+    private class Layer {
+        private double[][] weights;
+        private double[] nodes;
+        private double[] output;
+
+        public Layer(int size) {
+            nodes = new double[size];
+        }
+
+        /*
+            First pass through, returns getOutput() so you can create the other
+            layers. If nextLayerSize == 0, this is the last layer and input is
+            returned.
+         */
+        public double[] init(double[] input, int nextLayerSize) {
+            //if last layer return input
+            if (nextLayerSize > 0) {
+                weights = new double[input.length][nextLayerSize];
+
+                //randomize weights
+                for (int i = 0; i < weights.length; i++) {
+                    for (int j = 0; j < weights[0].length; j++) {
+                        weights[i][j] = Math.random() * 1;
+                    }
+                }
             }
-            //do sigmoid
+            else {
+                return input;
+            }
 
-            return 0;
+            nodes = input;
+
+            return getOutput();
+        }
+
+        /*
+            Gets an array of output[] that corresponds to the input
+            to the next layer if it exists
+         */
+        public double[] getOutput() {
+            //some matrix multiplication
+            //[output] = [weights] * [nodes]
+            for (int i= 0; i< weights.length; i++) {
+                for (int col = 0; col < weights[0].length; col++) {
+                    output[col] += weights[col][i] * nodes[col];
+                }
+            }
+
+            //sigmoid( [outputs] )
+            for (int i= 0; i< output.length; i++) {
+                output[i] = sigmoid(output[i]);
+            }
+
+            return output;
+        }
+
+        public double sigmoid(double z) {
+            return 1 / (Math.exp(-1 * z));
         }
     }
 
-    public int numInputs;
-    public int numHiddenLayers;
-    public int numHiddenNodes;
-    public int numOutputs;
+    public double[] weights;
+    public Layer[] network;
     public DataC[] data;
 
-    public FeedForwardNet(int numInputs, int numHiddenLayers, int numHiddenNodes, int numOutputs) {
-        this.numInputs = numInputs;
-        this.numHiddenLayers = numHiddenLayers;
-        this.numHiddenNodes = numHiddenNodes;
-        this.numOutputs = numOutputs;
+    /*
+        Takes an array of layer sizes for the network i.e
+        [4, 2, 1] is a 3 layer net with 4 nodes in 1st layer,
+        2 in 2nd layer, etc.
+     */
+    public FeedForwardNet(int[] layers) {
+
     }
 
-    public FeedForwardNet(DataC[] data) {
-        this.data = data;
-        new FeedForwardNet(data[0].getFeatures().length, 1, 10, 1);
-    }
 
     //make input nodes, hidden nodes, output nodes, randomize weights
     public void init() {
 
     }
 
-    //push add data samples through the net
+    //push data samples through the net
     public void feedForward() {
 
     }
