9e453e707308831f8f5239460a2b863b86fdd591
Dave Miller <superdave2347@gmail.com>
2020-10-12 17:56:53 -0600

    Network is set up and working, basically an array of Layer objects. Need to add backprop()

--- project3/project3/FeedForwardNet.java
+++ project3/project3/FeedForwardNet.java
@@ -3,11 +3,13 @@ package project3;
 import java.util.Arrays;
 
 /*
-    Need to figure out how to make a nice architecture of a network
-    Node class might work, but I will give it some thought.
+    A feed forward neural network
+
+    @Author: Dave Miller
  */
 public class FeedForwardNet {
 
+    //===============================================>LAYER
     /*
         Layer describes a single layer in the network
 
@@ -32,34 +34,35 @@ public class FeedForwardNet {
         public double[] init(double[] input, int nextLayerSize) {
             //if last layer return input
             if (nextLayerSize > 0) {
-                weights = new double[input.length][nextLayerSize];
+                weights = new double[nextLayerSize][input.length];
+                output = new double[nextLayerSize];
 
                 //randomize weights
-                for (int i = 0; i < weights.length; i++) {
-                    for (int j = 0; j < weights[0].length; j++) {
-                        weights[i][j] = Math.random() * 1;
+                for (int row = 0; row < weights.length; row++) {
+                    for (int col = 0; col < weights[0].length; col++) {
+                        weights[row][col] = Math.random()*2 -1;
                     }
                 }
+                return feedForward(input);
             }
             else {
                 return input;
             }
-
-            nodes = input;
-
-            return getOutput();
         }
 
         /*
             Gets an array of output[] that corresponds to the input
             to the next layer if it exists
+
+            This should not be called on the output layer
          */
-        public double[] getOutput() {
+        public double[] feedForward(double[] input) {
+            this.nodes = input;
             //some matrix multiplication
             //[output] = [weights] * [nodes]
             for (int i= 0; i< weights.length; i++) {
-                for (int col = 0; col < weights[0].length; col++) {
-                    output[col] += weights[col][i] * nodes[col];
+                for (int k= 0; k< weights[0].length; k++) {
+                    output[i] += weights[i][k] * nodes[k];
                 }
             }
 
@@ -72,37 +75,106 @@ public class FeedForwardNet {
         }
 
         public double sigmoid(double z) {
-            return 1 / (Math.exp(-1 * z));
+            return 1 / (1 + Math.exp(-1 * z));
         }
     }
 
-    public double[] weights;
+
+    //========================================>FEED FORWARD NET
+
     public Layer[] network;
     public DataC[] data;
+    public double eta;
 
     /*
         Takes an array of layer sizes for the network i.e
         [4, 2, 1] is a 3 layer net with 4 nodes in 1st layer,
         2 in 2nd layer, etc.
+
+        For classification, layers should be [numFeatures, ..., numClasses]
      */
-    public FeedForwardNet(int[] layers) {
+    public FeedForwardNet(DataC[] trainingData, int[] layers) {
+        this.data = trainingData;
+        network = new Layer[layers.length];
 
+        //for each layer, make a Layer object of size layers[i]
+        for (int i= 0; i< layers.length; i++) {
+            network[i] = new Layer(layers[i]);
+        }
+
+        init();
     }
 
 
-    //make input nodes, hidden nodes, output nodes, randomize weights
+    //call Layer.init(), first pass through the network
     public void init() {
+        //init() first layer with DataC[] input
+        double[] output;
+        output = network[0].init(data[0].getNormalizedFeatures(), network[1].nodes.length);
 
+        //hidden layer(s)
+        for (int i= 1; i< network.length-1; i++) {
+            output = network[i].init(output, network[i+1].nodes.length);
+        }
+
+        //output layer
+        network[network.length-1].init(output, 0);
     }
 
-    //push data samples through the net
-    public void feedForward() {
+    /*
+        Push 1 data sample through the network,
+        returns output layer
+     */
+    public double[] feedForward(double[] in) {
+        //first layer
+        double[] output;
+        output = network[0].feedForward(in);
 
+        //hidden layer(s)
+        for (int i= 1; i< network.length-1; i++) {
+            output = network[i].feedForward(output);
+        }
+
+        //output layer
+        return output;
     }
 
     //update weights
     public void backprop() {
+        for (DataC d: data) {
+            double[] output = feedForward(d.getNormalizedFeatures());
+            int target = Integer.parseInt(d.getClassLabel())-1;
+            double[] t = new double[output.length];
+            Arrays.fill(t, 0.0);
+            t[target] = 1.0;
 
+            System.out.println(cost(output, t));
+        }
+    }
+
+    /*
+        Gives squared error
+     */
+    public double cost(double[] output, double[] target) {
+        double sum= 0;
+
+        for (int i= 0; i< output.length; i++) {
+            sum += Math.pow(output[i] - target[i], 2);
+        }
+
+        return sum / 2;
+    }
+
+
+    //==================================>MAIN
+    public static void main(String[] args) {
+        DataSetUp glass = new DataSetUp("data-sets/glass.data", "end", "classification");
+        glass.zScoreNormalize();
+
+        int inputLen = glass.getAllData()[0].getNormalizedFeatures().length;
+        int[] layers = {inputLen, 5, 7};
+        FeedForwardNet network = new FeedForwardNet(glass.getAllData(), layers);
+        network.backprop();
     }
 
 }
